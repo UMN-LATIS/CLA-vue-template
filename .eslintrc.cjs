74070d90a4b5735e842bf7d088d21e8d5372e748
James Johnson <johnsojr@umn.edu>
2023-01-23 22:53:36 -0600

    update to storybook 7 with vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    "vue/setup-compiler-macros": true,
+    "vue/setup-compiler-macros": true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential",
-    "@vue/eslint-config-typescript",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential", "@vue/eslint-config-typescript", "prettier", "plugin:storybook/recommended"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["vue", "@typescript-eslint"],
   rules: {
@@ -25,13 +20,9 @@ module.exports = {
     "vue/attribute-hyphenation": ["error", "never"],
     "vue/v-on-event-hyphenation": ["error", "never"],
     "vue/component-definition-name-casing": "off",
-    "vue/component-name-in-template-casing": [
-      "warn",
-      "PascalCase",
-      {
-        registeredComponentsOnly: false,
-        ignores: ["component", "router-link", "router-view"],
-      },
-    ],
-  },
+    "vue/component-name-in-template-casing": ["warn", "PascalCase", {
+      registeredComponentsOnly: false,
+      ignores: ["component", "router-link", "router-view"]
+    }]
+  }
 };
--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-stories: ['../src/components/**/*.stories.js'],
- addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-};
