b3d6302d450f296eb467fee2bf91e358f5d25e74
James Johnson <johnsojr@umn.edu>
2023-01-24 00:16:52 -0600

    npm build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  // config for building the npm library of vue components
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@umn-latis/cla-vue-template",
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+    sourcemap: true,
+  },
 });
