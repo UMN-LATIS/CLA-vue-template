ad233e873f5f297dd91537b1a3bb248b7aa09b2d
James Johnson <johnsojr@umn.edu>
2023-01-23 21:05:22 -0600

    WIP rollup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { default as AppFooter } from "./components/AppFooter.vue";
+export { default as AppHeader } from "./components/AppHeader.vue";
+export { default as Navbar } from "./components/Navbar.vue";
+export { default as NavbarItem } from "./components/NavbarItem.vue";
+export { default as PostIt } from "./components/PostIt.vue";
+export { default as CollegeHeader } from "./components/CollegeHeader.vue";
+export { default as CLAWordmark } from "./components/CLAWordmark.vue";
+export { default as UMNWordmark } from "./components/UMNWordmark.vue";
+export { default as UniversityHeader } from "./components/UniversityHeader.vue";
+
+import "./index.css";
