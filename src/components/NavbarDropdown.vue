d73feac6cda060b618b13116ad2b8d122d2bee2d
James Johnson <johnsojr@umn.edu>
2023-01-30 16:43:19 -0600

    Dropdown

--- src/components/NavbarDropdown.vue
+++ src/components/NavbarDropdown.vue
@@ -1,10 +1,20 @@
 <template>
-  <NavbarItem class="navbar-dropdown">
-    <button @click="isOpen = !isOpen" class="navbar-dropdown__label-button">
+  <NavbarItem ref="itemContainer">
+    <button
+      @click="isOpen = !isOpen"
+      class="flex w-full h-full px-5 items-center gap-2 whitespace-nowrap"
+      :class="{
+        'text-umn-maroon bg-white': isOpen,
+        'text-umn-neutral-700': !isOpen,
+      }"
+    >
       {{ label }}
       <Icons.ChevronDown />
     </button>
-    <ul v-if="isOpen" class="navbar-dropdown__items">
+    <ul
+      v-if="isOpen"
+      class="bg-white absolute top-full left-0 w-64 -mt-1 shadow-md"
+    >
       <slot></slot>
     </ul>
   </NavbarItem>
@@ -13,10 +23,19 @@
 import NavbarItem from "./NavbarItem.vue";
 import * as Icons from "../icons";
 import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+
 defineProps<{
   label: string;
 }>();
 
 const isOpen = ref(false);
+const itemContainer = ref<InstanceType<typeof NavbarItem> | null>(null);
+
+onClickOutside(itemContainer, () => {
+  console.log("clicked outside");
+  if (!itemContainer.value) return;
+  isOpen.value = false;
+});
 </script>
 <style scoped></style>
