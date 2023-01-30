319882a3ee6598c2a1fa4d79ab65d76a4271514f
James Johnson <johnsojr@umn.edu>
2023-01-30 13:37:56 -0600

    add colors and base

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'tw-',
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [
-    require('postcss-nested-import')
-  ],
-  corePlugins: {
-    // disabling preflight so that we can adopt tailwind's classes
-    // on a case-by-case basis with the `tw-preflight`
-    preflight: false,
+    extend: {
+      colors: {
+        "umn-maroon": "#7a0019",
+        "umn-gold": "#ffcc33",
+        "umn-gold-light": "#ffde7a",
+        "umn-neutral-50": "rgb(249, 247, 246)",
+        "umn-neutral-100": `#f0efee`,
+        "umn-neutral-200": `#d5d6d2`,
+        "umn-neutral-500": `#737487`,
+        "umn-neutral-700": `#5a5a5a`,
+        "umn-neutral-800": `#262626`,
+        "umn-neutral-900": `#1a1a1a`,
+      },
+    },
   },
 };
