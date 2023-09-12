2a1c6504eba1a41fafb145638441134c1636aa52
James Johnson <johnsojr@umn.edu>
2023-09-12 14:37:27 -0500

    add story for larger menu

--- src/components/AppHeader.stories.js
+++ src/components/AppHeader.stories.js
@@ -1,24 +1,23 @@
-import AppHeader from './AppHeader.vue';
-import NavbarItem from './NavbarItem.vue';
-import NavbarDropdown from './NavbarDropdown.vue';
-
+import AppHeader from "./AppHeader.vue";
+import NavbarItem from "./NavbarItem.vue";
+import NavbarDropdown from "./NavbarDropdown.vue";
 
 export default {
   component: AppHeader,
-  title: 'AppHeader',
+  title: "AppHeader",
   argTypes: {
     appslot: {
-            type: "string",
-            default: "Default Slot Text",
+      type: "string",
+      default: "Default Slot Text",
     },
     navslot: {
-            type: "string",
-            default: "Default Slot Text",
+      type: "string",
+      default: "Default Slot Text",
     },
   },
 };
 
-const Template = args => ({
+const Template = (args) => ({
   components: { AppHeader, NavbarItem, NavbarDropdown },
   setup() {
     return { args };
@@ -28,14 +27,14 @@ const Template = args => ({
 
 export const Default = Template.bind({});
 Default.args = {
-    appslot: '<a href="/">MyLink</a>',
-    navslot: `
+  appslot: '<a href="/">MyLink</a>',
+  navslot: `
       <NavbarItem class="active"><a href="/">Home</a></NavbarItem>
       <NavbarItem><a href="/">Away</a></NavbarItem>
-      <NavbarItem><a href="/">Really Away</a></NavbarItem>`
+      <NavbarItem><a href="/">Really Away</a></NavbarItem>`,
 };
 
-const Template2 = args => ({
+const Template2 = (args) => ({
   components: { AppHeader, NavbarItem },
   setup() {
     return { args };
@@ -43,18 +42,19 @@ const Template2 = args => ({
   template: `<AppHeader><template #app-link>${args.appslot}</template><template #navbar-links>${args.navslot}</template><template #navbar-links-right>${args.navslotRight}</template></AppHeader>`,
 });
 
-
 export const SplitNav = Template2.bind({});
 SplitNav.args = {
-    appslot: '<a href="/">MyLink</a>',
-    navslot: '<NavbarItem class="active"><a href="/">Home</a></NavbarItem><NavbarItem><a href="/">Away</a></NavbarItem><NavbarItem><a href="/">Really Away</a></NavbarItem>',
-    navslotRight: '<NavbarItem><a href="/">Help</a></NavbarItem><NavbarItem><a href="/">Logout</a></NavbarItem>'
+  appslot: '<a href="/">MyLink</a>',
+  navslot:
+    '<NavbarItem class="active"><a href="/">Home</a></NavbarItem><NavbarItem><a href="/">Away</a></NavbarItem><NavbarItem><a href="/">Really Away</a></NavbarItem>',
+  navslotRight:
+    '<NavbarItem><a href="/">Help</a></NavbarItem><NavbarItem><a href="/">Logout</a></NavbarItem>',
 };
 
 export const WithDropdownItems = Template.bind({});
 WithDropdownItems.args = {
-    ...Default.args,
-    navslot: `
+  ...Default.args,
+  navslot: `
       <NavbarItem><a href="#">Item</a></NavbarItem>
       <NavbarDropdown label="Dropdown 1">
         <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
@@ -66,5 +66,24 @@ WithDropdownItems.args = {
         <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
         <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
       </NavbarDropdown>
-      `
+      `,
+};
+
+export const WithLargeMenu = Template.bind({});
+WithLargeMenu.args = {
+  ...Default.args,
+  breakpoint: "lg",
+  navslot: `
+      <NavbarItem v-for="idx in 8"><a href="#">My Nav Item {{ idx }} </a></NavbarItem>
+      <NavbarDropdown label="Dropdown 1">
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+      </NavbarDropdown>
+      <NavbarDropdown label="Dropdown 2">
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+        <NavbarItem><a href="#">Dropdown Item</a></NavbarItem>
+      </NavbarDropdown>
+      `,
 };
