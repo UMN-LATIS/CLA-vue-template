b34cab1f849f516743a5e5ee4c0385e846f77da8
James Johnson <johnsojr@umn.edu>
2023-09-12 17:21:00 -0500

    none should always show mobile menu

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
 import type { InjectionKey } from "vue";
 
 export const BREAKPOINTS = {
-  none: 0,
   sm: 640,
   md: 768,
   lg: 1024,
   xl: 1280,
+  none: Infinity,
 } as const;
 
 export const menuBreakpointInjectionKey = Symbol(
