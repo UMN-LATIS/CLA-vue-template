8bc3f992e36696d40bc58e509e5842f51f25e641
James Johnson <johnsojr@umn.edu>
2025-09-16 10:40:31 -0500

    add dropdown menu in demo app

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
         <NavbarItem class="active"><a href="/">Home</a></NavbarItem>
         <NavbarItem><a href="/">Away</a></NavbarItem>
         <NavbarItem><a href="/">Really Away</a></NavbarItem>
+        <NavbarDropdown label="Dropdown">
+            <NavbarItem><a href="/">Link 1</a></NavbarItem>
+            <NavbarItem><a href="/">Link 2</a></NavbarItem>
+            <NavbarItem><a href="/">Link 3</a></NavbarItem>
+        </NavbarDropdown>
       </template>
     </AppHeader>
     <PostIt class="tw-flex-1">
@@ -25,6 +30,7 @@ import AppHeader from "./components/AppHeader.vue";
 import NavbarItem from "./components/NavbarItem.vue";
 import PostIt from "./components/PostIt.vue";
 import AppFooter from "./components/AppFooter.vue";
+import NavbarDropdown from "./components/NavbarDropdown.vue";
 </script>
 
 <style>
